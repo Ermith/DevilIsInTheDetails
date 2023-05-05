f665f8bba74f46c00eca3c7175270453796a5150
Ermith <f.s.ermith@gmail.com>
2023-05-05 22:40:54 +0200

    Health is now separate script

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -1,21 +1,6 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Health))]
 class Hero : MonoBehaviour
 {
-    public int MaxHealth = 100;
-    public int Health { get; private set; }
-
-    private void Awake()
-    {
-        Health = MaxHealth;
-    }
-
-    public void HitBy(int damage, GameObject attacker)
-    {
-        Health -= damage;
-        if (Health <= 0)
-        {
-            Destroy(gameObject);
-        }
-    }
 }
