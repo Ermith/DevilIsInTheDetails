6694f27f4aa57a4bfe5ff4a194cf426b04cf5c5d
pali <6pali6@gmail.com>
2023-05-05 21:35:18 +0200

    word manager

---
+++ Assets/New Folder/WordManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class WordManager : MonoBehaviour
+{
+    public readonly TextAsset WordFile;
+
+    private readonly HashSet<string> _words = new();
+
+    private readonly  Dictionary<char, int> _letterFrequency = new();
+
+    private int _totalLetters;
+
+    private void Awake()
+    {
+        // read all words from the file
+        string[] lines = WordFile.text.Split('\n');
+        foreach (string line in lines)
+        {
+            _words.Add(line.Trim());
+            foreach (char c in line)
+            {
+                if (_letterFrequency.ContainsKey(c))
+                {
+                    _letterFrequency[c]++;
+                }
+                else
+                {
+                    _letterFrequency.Add(c, 1);
+                }
+                _totalLetters++;
+            }
+        }
+    }
+
+    public bool IsWord(string word)
+    {
+        return _words.Contains(word);
+    }
+
+    public char GetLetter()
+    {
+        // weighted pick based on letter frequencies
+        int r = Random.Range(0, _totalLetters);
+        foreach (KeyValuePair<char, int> pair in _letterFrequency)
+        {
+            r -= pair.Value;
+            if (r <= 0)
+            {
+                return pair.Key;
+            }
+        }
+        throw new System.Exception("Should never happen");
+    }
+}
