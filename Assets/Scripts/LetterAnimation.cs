f3c2f4aeba57066752b6d10df914b44cdc8f7056
Ermith <f.s.ermith@gmail.com>
2023-05-06 15:23:20 +0200

    Letter Disappearing Animations

--- Assets/Scripts/LetterAnimation.cs
+++ Assets/Scripts/LetterAnimation.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 [RequireComponent(typeof(Animation))]
 public class LetterAnimation : MonoBehaviour
@@ -8,14 +9,34 @@ public class LetterAnimation : MonoBehaviour
     private Animation _animation;
     // Start is called before the first frame update
 
-    public void PlayAppearing()
+    public void PlayAppearing(Action onEnd = null)
     {
-        _animation.Play("Letter Appearing");
+        string name = "LetterAppearing";
+        _animation.Play(name);
+
+        if (onEnd != null)
+            StartCoroutine(name, onEnd);
+    }
+
+    public void PlayDisappearing(Action onEnd = null)
+    {
+        string name = "LetterDisappearing";
+        _animation.Play(name);
+
+        if (onEnd != null)
+            StartCoroutine(WhatForEnd(name, onEnd));
+    }
+
+    public IEnumerator WhatForEnd(string name, Action onEnd)
+    {
+        while (_animation.IsPlaying(name))
+            yield return null;
+
+        onEnd();
     }
 
     void Start()
     {
         _animation = GetComponent<Animation>();
-        //PlayAppearing();
     }
 }
