30fa6c23c4ba332601acd0b33426a2621af1b9a7
pali <6pali6@gmail.com>
2023-05-06 13:19:52 +0200

    begone, devil named \r

--- Assets/Scripts/WordManager.cs
+++ Assets/Scripts/WordManager.cs
@@ -17,9 +17,9 @@ public class WordManager : MonoBehaviour
         string[] lines = WordFile.text.Split('\n');
         foreach (string line in lines)
         {
-            line = line.Trim();
-            _words.Add(line);
-            foreach (char c in line)
+            string word = line.Trim();
+            _words.Add(word);
+            foreach (char c in word)
             {
                 if (_letterFrequency.ContainsKey(c))
                 {
