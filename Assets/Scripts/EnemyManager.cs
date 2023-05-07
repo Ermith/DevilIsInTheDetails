01ef0f9617a6040cdd867cd13d4539abaf84f77d
pali <6pali6@gmail.com>
2023-05-07 11:47:02 +0200

    enemy manager

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,50 @@
+﻿using DG.Tweening;
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class EnemyManager : MonoBehaviour
+{
+    public UDictionary<Enemy, int> EnemySpawnWeights;
+
+    [SerializeField] private Transform _enemySpawn;
+
+    private Enemy PickEnemy()
+    {
+        int totalWeight = 0;
+        foreach (var enemy in EnemySpawnWeights)
+        {
+            totalWeight += enemy.Value;
+        }
+
+        int randomWeight = Random.Range(0, totalWeight);
+        foreach (var enemy in EnemySpawnWeights)
+        {
+            randomWeight -= enemy.Value;
+            if (randomWeight <= 0)
+            {
+                return enemy.Key;
+            }
+        }
+
+        return null;
+    }
+
+    public Enemy SpawnEnemy()
+    {
+        Enemy enemy = Instantiate(PickEnemy());
+        
+        enemy.transform.position = _enemySpawn.position + Vector3.right * Random.Range(3f, 5f);
+        SpriteRenderer spriteRenderer = enemy.GetComponent<SpriteRenderer>();
+        spriteRenderer.color = new Color(1f, 1f, 1f, 0f);
+
+        spriteRenderer.DOColor(Color.white, 0.5f).SetEase(Ease.OutBack);
+        enemy.transform.DOMoveX(_enemySpawn.position.x, 0.5f).SetEase(Ease.OutBack);
+
+        enemy.GetComponent<Health>().SetupHealthbar();
+        RectTransform rt = enemy.GetComponent<Health>().Healthbar.GetComponent<RectTransform>();
+        rt.DOAnchorPos((Vector2)_enemySpawn.transform.position + Vector2.up * 2f, 0.5f).SetEase(Ease.OutBack);
+
+        return enemy;
+    }
+}
--- Assets/Scripts/GameDirector.cs
+++ Assets/Scripts/GameDirector.cs
@@ -11,11 +11,9 @@ public class GameDirector : MonoBehaviour
     //public const string InventoryName = "Inventory";
     //public const string WordManagerName = "WordManager";
     //public const string GameDirectorName = "GameDirector";
-
-    [SerializeField] private Enemy _enemyPrefab;
+    
     [SerializeField] private Hero _heroPrefab;
     [SerializeField] private Transform _heroSpawn;
-    [SerializeField] private Transform _enemySpawn;
 
     public static Hero HeroInstance { get; private set; }
     public static Enemy EnemyInstance { get; private set; }
@@ -23,6 +21,8 @@ public class GameDirector : MonoBehaviour
     public static GameDirector GameDirectorInstance { get; private set; }
     public static Inventory InventoryInstance { get; private set; }
     public static ItemManager ItemManagerInstance { get; private set; }
+
+    public static EnemyManager EnemyManagerInstance { get; private set; }
 
     public static float SimulationTime = 0f;
 
@@ -56,18 +56,7 @@ public class GameDirector : MonoBehaviour
     {
         if (_fighting) return;
         _fighting = true;
-
-        EnemyInstance = Instantiate(_enemyPrefab);
-        EnemyInstance.transform.position = _enemySpawn.position + Vector3.right * Random.Range(3f, 5f);
-        SpriteRenderer spriteRenderer = EnemyInstance.GetComponent<SpriteRenderer>();
-        spriteRenderer.color = new Color(1f, 1f, 1f, 0f);
-
-        spriteRenderer.DOColor(Color.white, 0.5f).SetEase(Ease.OutBack);
-        EnemyInstance.transform.DOMoveX(_enemySpawn.position.x, 0.5f).SetEase(Ease.OutBack);
-
-        EnemyInstance.GetComponent<Health>().SetupHealthbar();
-        RectTransform rt = EnemyInstance.GetComponent<Health>().Healthbar.GetComponent<RectTransform>();
-        rt.DOAnchorPos((Vector2)_enemySpawn.transform.position + Vector2.up * 2f, 0.5f).SetEase(Ease.OutBack);
+        EnemyInstance = EnemyManagerInstance.SpawnEnemy();
     }
 
     void Update()
@@ -136,6 +125,7 @@ public class GameDirector : MonoBehaviour
         WordManagerInstance = FindObjectOfType<WordManager>();
         InventoryInstance = FindObjectOfType<Inventory>();
         ItemManagerInstance = FindObjectOfType<ItemManager>();
+        EnemyManagerInstance = FindObjectOfType<EnemyManager>();
 
         // HERO
         HeroInstance = Instantiate(_heroPrefab);
